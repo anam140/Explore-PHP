d328558c78d067385fb6e2eb7cd716be698cab8f
ANam <50522741+anam140@users.noreply.github.com>
2020-10-13 17:39:34 +0530

    Add files via upload
     This a beginner's program for PHP development
     /* *
     * Learn How to: 
     * 1.  Print Simply Hello All!
     * 2. How to Concatenate Strings
     * 3. Perform Arithmetic Operations [Addition, Subtraction Multiplication, Division]
     * 4. Add Line Break, Single Line Comments as well as Multiple Line Comments
     *  */

---
+++ basics.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Multiple Line Comments 
+ * This a beginner's program for PHP development
+ * Learn How to: 
+ * 1. Print Simply Hello All!
+ * 2. Concatenate Strings
+* 3. Perform Arithmetic Operations [Addition, Subtraction Multiplication, Division]
+ * 4. Add Line Break, Single Line Comments as well as Multiple Line Comments
+ *  */ 
+echo "Hello All!"; 
+# Output : Hello All!
+
+echo "<br>"; // Line Break!
+
+echo "Hello" .' '. 'All';
+# Output : Hello All! using Concatenation
+echo "<br>";
+
+// Operations in PHP 
+echo "Lets do some calculations";
+echo "<br>";
+
+// Addition
+echo 27+34;
+#Output 61
+echo "<br>";
+
+// Substraction
+echo 61-27;
+#Output 35
+echo "<br>";
+
+// Multiplication
+echo 10*6;
+#Output 60
+echo "<br>";
+
+// Division
+echo 10/2;
+#Output 5
+echo "<br>";
+ ?>
